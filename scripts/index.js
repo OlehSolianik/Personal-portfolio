0cb0839645c7e7a176836f1373e89115a2f77151
Oleh Solianik <olehsolianik@gmail.com>
2025-08-17 21:01:39 +0300

    Website banner style and layout refinements

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+/*let lastScrollY = window.scrollY;
+const websiteBanner = document.querySelector('.website-banner'); 
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY; 
+  if (currentScrollY > lastScrollY && currentScrollY > 50) {
+    websiteBanner.classList.add('website-banner-hidden');
+  }
+  else if (currentScrollY < lastScrollY) {
+      header.classList.remove('website-banner-hidden');
+  }
+  lastScrollY = currentScrollY;
+});*/
