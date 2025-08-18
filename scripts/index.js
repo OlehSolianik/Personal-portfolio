49b267eac03a5b2bcd0b2e5a6e50935ff074a652
Oleh Solianik <olehsolianik@gmail.com>
2025-08-18 20:02:52 +0300

    Scroll-based visibility toggle for website banner

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
-/*let lastScrollY = window.scrollY;
-const websiteBanner = document.querySelector('.website-banner'); 
-window.addEventListener('scroll', () => {
-  const currentScrollY = window.scrollY; 
-  if (currentScrollY > lastScrollY && currentScrollY > 50) {
-    websiteBanner.classList.add('website-banner-hidden');
+const websiteBanner = document.querySelector('.website-banner');
+const bannerTrigger = document.querySelector('.banner-trigger'); 
+
+let lastScrollPosition = window.scrollY;
+let currentScrollDirection = null;
+let scrollTimer = null;
+const scrollDuration = 100; 
+
+window.onscroll = function() {
+  const currentScrollPosition = window.scrollY; 
+  const changedScrollDirection = currentScrollPosition > lastScrollPosition ? "down" : "up";
+  const triggerOffsetTop = bannerTrigger.offsetTop;
+  console.log(triggerOffsetTop);
+
+  if (changedScrollDirection !== currentScrollDirection) {
+    currentScrollDirection = changedScrollDirection;
+    clearTimeout(scrollTimer);
+
+    scrollTimer = setTimeout(() => {
+      if (currentScrollDirection === "down") {
+          websiteBanner.style.transform = "translateY(0)";
+      } 
+      else if (currentScrollDirection === "up") {
+        websiteBanner.style.transform = "translateY(0)";
+      }
+  }, scrollDuration);
   }
-  else if (currentScrollY < lastScrollY) {
-      header.classList.remove('website-banner-hidden');
+  else if (currentScrollDirection === "down" && currentScrollPosition >= triggerOffsetTop) {
+     websiteBanner.style.transform = "translateY(-100%)";
   }
-  lastScrollY = currentScrollY;
-});*/
+  lastScrollPosition = currentScrollPosition;
+};
